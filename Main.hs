749917e84309007d6d25d86da029b64a27bf9320
codygman <cody@codygman.dev>
2021-09-30 14:20:52 -0500

    try simplifying, little different approach

--- Main.hs
+++ Main.hs
@@ -15,12 +15,8 @@ import qualified Database.HDBC      as HDBC
 import qualified Data.Convertible as Convertible
 import qualified Data.Text as Text
 
-data EnvironmentWith context = EnvironmentWith
-  { environmentContext :: context
-  }
-
-newtype App context m a =
-  App { runApp :: Reader.ReaderT (EnvironmentWith context) m a }
+newtype App dbPool m a =
+  App { runApp :: Reader.ReaderT dbPool m a }
   deriving
   ( Applicative
   , Functor
@@ -30,9 +26,10 @@ newtype App context m a =
   , Reader.MonadIO
   , MonadFail
   , Resource.MonadThrow
+  , Reader.MonadTrans
   )
 
-newtype LoggingOrville c m a =
+newtype LoggingOrville conn m a =
   LoggingOrville { runLoggingOrville :: m a }
   deriving (Applicative
            , Monad
@@ -44,63 +41,67 @@ newtype LoggingOrville c m a =
            , Resource.MonadThrow
            )
 
+instance Orville.MonadOrville Postgres.Connection (LoggingOrville Postgres.Connection IO) where
+    getOrvilleEnv = do
+      dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) HDBC.disconnect 1 60 1
+      let env = Orville.newOrvilleEnv dbPool
+      pure env
+    localOrvilleEnv f = id
+      -- LoggingOrville
+      -- . Reader.local
+      --     (\localEnv -> localEnv)
+      -- . runLoggingOrville
+    runningQuery _ sql query = do
+      query
+
+
 -- https://taylor.fausak.me/orville/Database-Orville-Core.html#t:MonadOrville
--- I can provide instance for my App monad
-instance (HDBC.IConnection conn, Orville.MonadOrville conn m) => Orville.MonadOrville Postgres.Connection (App String m) where
-  getOrvilleEnv = do
-    dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) (HDBC.disconnect) 1 60 1
-    let env = Orville.newOrvilleEnv dbPool
-    pure env
-  localOrvilleEnv f =
-    App
-      . Reader.local
-          (\localEnv -> localEnv)
-      . runApp
-  runningQuery _ sql query = do
-    query
-
-instance (Reader.MonadIO m, Resource.MonadThrow m, Control.MonadBaseControl IO m) => Orville.MonadOrville Postgres.Connection (LoggingOrville Postgres.Connection (App String m)) where
-  getOrvilleEnv = do
-    dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) (HDBC.disconnect) 1 60 1
-    let env = Orville.newOrvilleEnv dbPool
-    pure env
-  localOrvilleEnv f = id -- not sure why below doesn't work
-    -- LoggingOrville
-    --   . Reader.local
-    --       (\localEnv -> localEnv)
-    --   . runLoggingOrville
-  runningQuery _ sql query = do
-    error "hi"
-    Reader.liftIO $ putStrLn sql
-    query
-
-runLoggingQuery :: (MonadFail m, Resource.MonadThrow m, Reader.MonadIO m, HDBC.IConnection conn, Orville.MonadOrville conn m) =>  LoggingOrville Postgres.Connection m [String]
-runLoggingQuery = do
+loggingOrvilleExSelect :: (MonadFail m, Resource.MonadThrow m, Reader.MonadIO m, HDBC.IConnection conn, Orville.MonadOrville conn m) =>  LoggingOrville Postgres.Connection m [String]
+loggingOrvilleExSelect = do
   LoggingOrville $ do
     Orville.selectSql @String
       "select aggnumdirectargs from pg_catalog.pg_aggregate pa limit 2;"
       []
       (Orville.col @Text.Text "aggnumdirectargs")
 
-runNormalQuery :: (MonadFail m, Resource.MonadThrow m, Reader.MonadIO m, HDBC.IConnection conn, Orville.MonadOrville conn m) =>  App String m [String]
-runNormalQuery = do
-  App $ do
-    Orville.selectSql @String
-      "select aggnumdirectargs from pg_catalog.pg_aggregate pa limit 2;"
-      []
-      (Orville.col @Text.Text "aggnumdirectargs")
+loggingAppExSelect ::App Postgres.Connection (LoggingOrville Postgres.Connection IO) [String]
+loggingAppExSelect = loggingAppExSelect
 
 main :: IO ()
 main = do
-  dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) (HDBC.disconnect) 1 60 1
+  dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) HDBC.disconnect 1 60 1
   Reader.liftIO $ putStrLn "pool created"
-  let env = Orville.newOrvilleEnv dbPool
-  --  print =<< Reader.runReaderT ((flip Orville.runOrville env) . runApp runNormalQuery) (EnvironmentWith "tst")
-  Reader.liftIO $  putStrLn "env created"
+  let runStuff :: Reader.ReaderT (Pool.Pool Postgres.Connection) (LoggingOrville Postgres.Connection IO) String
+      runStuff = runLoggingOrville . flip Reader.runReaderT dbPool $  do
+        runApp loggingAppExSelect
+  print @[String] =<< runStuff
 
-   -- ugh this one is using the default implementation :/
-  print @[String] =<< (flip Orville.runOrville env . flip Reader.runReaderT (EnvironmentWith "") . runApp) (Orville.withTransaction runNormalQuery)
+-- old
+-- I can provide instance for my App monad
+-- instance (HDBC.IConnection conn, Orville.MonadOrville conn m) => Orville.MonadOrville Postgres.Connection (App String m) where
+--   getOrvilleEnv = do
+--     dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) (HDBC.disconnect) 1 60 1
+--     let env = Orville.newOrvilleEnv dbPool
+--     pure env
+--   localOrvilleEnv f =
+--     App
+--       . Reader.local
+--           (\localEnv -> localEnv)
+--       . runApp
+--   runningQuery _ sql query = do
+--     query
 
-  -- This one is using the one that isn't supposed to be logging
-  -- so it worked by accident
-  print @[String] =<< (flip Orville.runOrville env . flip Reader.runReaderT (EnvironmentWith ("" :: String)) . runApp . runLoggingOrville) (Orville.withTransaction runLoggingQuery)
+-- instance (Reader.MonadIO m, Resource.MonadThrow m, Control.MonadBaseControl IO m) => Orville.MonadOrville Postgres.Connection (LoggingOrville Postgres.Connection (App String m)) where
+--   getOrvilleEnv = do
+--     dbPool  <- Reader.liftIO $ Pool.createPool (Postgres.connectPostgreSQL "postgresql://postgres@localhost:5432" ) (HDBC.disconnect) 1 60 1
+--     let env = Orville.newOrvilleEnv dbPool
+--     pure env
+--   localOrvilleEnv f = id -- not sure why below doesn't work
+--     -- LoggingOrville
+--     --   . Reader.local
+--     --       (\localEnv -> localEnv)
+--     --   . runLoggingOrville
+--   runningQuery _ sql query = do
+--     error "instance with logging"
+--     Reader.liftIO $ putStrLn sql
+--     query
